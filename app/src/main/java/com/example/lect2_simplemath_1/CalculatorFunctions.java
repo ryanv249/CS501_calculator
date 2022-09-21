6dd86e86b0624570c09a92bd8b5522ea535fdf3f
Oliver Samuels <osamuels@bu.edu>
2022-09-20 22:50:13 -0400

    Calculator Changes

---
+++ app/src/main/java/com/example/lect2_simplemath_1/CalculatorFunctions.java
@@ -0,0 +1,21 @@
+package com.example.lect2_simplemath_1;
+
+public class CalculatorFunctions {
+
+    public static double addition(Double num1, Double num2) {
+        return num1 + num2;
+    }
+    public static double subtraction(Double num1, Double num2){
+        return num1 - num2;
+    }
+    public static double mutliplication(Double num1, Double num2){
+        return num1 * num2;
+    }
+    public static double division(Double num1, Double num2){
+        return num1 / num2;
+    }
+    public static double modula(Double num1, Double num2){
+        return num1% num2;
+    }
+
+}
